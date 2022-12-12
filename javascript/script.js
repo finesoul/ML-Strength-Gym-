11b7e9d56aa4e34c946be78f4f6331e5408519b3
Faisul Hassan <faisul.hassan@gmail.com>
2022-12-13 06:57:57 +1000

    Add initial ml strength gym website
    This version is a completed website submitted for Assessment, Term 2.

---
+++ javascript/script.js
@@ -0,0 +1,24 @@
+// the following code is used to demonstrate the need for defer
+
+// Fetch the html for the component
+fetch("/html/components/nav.html")
+  .then((response) => response.text())
+  .then((component_html) => {
+    console.log(component_html);
+    // Find the empty elements on the page
+    let list_of_elements = document.getElementsByClassName("nav-component");
+    // FIll the empty elemenets with the colntents of the fetched html
+    for (let element of list_of_elements) {
+      element.innerHTML = component_html;
+    }
+  });
+
+fetch("/html/components/footer.html")
+  .then((response) => response.text())
+  .then((component_html) => {
+    console.log(component_html);
+    let list_of_elements = document.getElementsByClassName("footer-component");
+    for (let element of list_of_elements) {
+      element.innerHTML = component_html;
+    }
+  });
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    theme: {
+      colors: {
+        'darkforge': '#171717',
+        'darkerforge': '#28282B',
+        'matcha-light':'#A3B18A',
+        'matcha-md':'#588157',
+      },
+    },
+  }
